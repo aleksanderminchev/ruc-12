b3c7ab183a60fb9695b9a718442796fe615bbd2e
aleksan18 <83026219+aleksanderminchev@users.noreply.github.com>
2023-11-04 11:29:12 +0100

    Hllo world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ruc_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
