0331756d09404809323f1cda384ebfee7cf6c976
aleksan18 <83026219+aleksanderminchev@users.noreply.github.com>
2023-12-02 14:01:57 +0100

    Header styling

--- MoviesReact/movies-app/src/components/Header/Header.jsx
+++ MoviesReact/movies-app/src/components/Header/Header.jsx
@@ -4,17 +4,23 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 function Header() {
   const [searchText, setSearchText] = useState("");
   const [searchOpen, setSearchOpen] = useState(false);
   return (
     <AppBar position="static">
-      <Toolbar>
+      <Toolbar
+        sx={{
+          border: "1px solid #000",
+          background: "#D9D9D9",
+          boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+        }}
+      >
         <Button color="inherit" component={Link} to="/">
           Home
         </Button>
-        <Button color="inherit" component={Link} to="/movies" >
+        <Button color="inherit" component={Link} to="/movies">
           Movies
         </Button>
         <Button color="inherit" component={Link} to="/actor">
